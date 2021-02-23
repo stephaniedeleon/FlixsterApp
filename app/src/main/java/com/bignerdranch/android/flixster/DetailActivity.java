be222e52fcd03c859f6e53ce0922b296d3a86ab0
stephaniedeleon <stephanie.deleon516@gmail.com>
2021-02-22 22:36:37 -0800

    Reset the previous push & removed the API key

--- app/src/main/java/com/bignerdranch/android/flixster/DetailActivity.java
+++ app/src/main/java/com/bignerdranch/android/flixster/DetailActivity.java
@@ -23,7 +23,7 @@ import okhttp3.Headers;
 
 public class DetailActivity extends YouTubeBaseActivity {
 
-    public static final String YOUTUBE_API_KEY = "AIzaSyCxaiugIkqrfzP8pjLdmAstRptH2xMdgG8";
+    public static final String YOUTUBE_API_KEY = "";
     public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
     TextView tvTitle;
